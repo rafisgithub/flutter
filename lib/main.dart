cf5d82cf96c5bd79e53ea45ee1e8d716c186a026
Rafi <smrafi120@gmail.com>
2023-07-06 21:13:07 +0600

    Creating customButton on StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -16,8 +18,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return Center(
-          child: Text("Hello world"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            CustomButton("Create"),
+            SizedBox(
+              height: 10,
+              ),
+             CustomButton("Read"),
+             SizedBox(
+              height: 10,
+             ),
+              CustomButton("update"),
+              SizedBox(
+                height: 10,
+              ),
+               CustomButton("delete"),
+          ],)
         );
     
   }
 }
+
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  CustomButton(this.title);
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print("onTap");
+      },
+      child: Container(
+        height: 40,
+        width: 100,
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: Colors.blue
+        ),
+        child: Center(child: Text(title)),
+      ),
+    );
+  }
+    
+  }
