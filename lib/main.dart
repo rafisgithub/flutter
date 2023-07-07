4b06b0f617e1e01a22fa2fdfaad83f317234cc5d
Rafi <smrafi120@gmail.com>
2023-07-07 15:07:33 +0600

    GridView Widget using count constructor

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(MaterialApp(
-    title: "Stack Example",
+    title: "GridView Exampla",
     home: Scaffold(
-      appBar: AppBar(title: Text("Stack Example")),
+      appBar: AppBar(title: Text("All about GridView")),
       body: MyApp(),
     ),
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        // alignment: Alignment.center,
-        // overflow:overflow.visiblae
-        clipBehavior: Clip.none,
-        children: [
-          Container(
-            height: 200,
-            width: 200,
-            color: Colors.green,
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Color.fromARGB(255, 231, 223, 223),
-            ),
-          ),
-          Positioned(
-            bottom: -25,
-            right: 10,
-            child: Container(
-              height: 50,
-              width: 50,
-              color: const Color.fromARGB(255, 175, 76, 84),
-            ),
-          ),
-        ],
-      ),
-    );
+    return GridView.count(
+      crossAxisCount: 3,
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      children: [
+        Container(
+          color: Colors.green,
+          child: Text("Hi"),
+          alignment: Alignment.center,
+        ),
+        Container(
+          color: Colors.green,
+          child: Text("Hi"),
+          alignment: Alignment.center,
+        ),
+        Container(
+          color: Colors.green,
+          child: Text("Hi"),
+          alignment: Alignment.center,
+        ),
+        Container(
+          color: Colors.green,
+          child: Text("Hi"),
+          alignment: Alignment.center,
+        ),
+        Container(
+          color: Colors.green,
+          child: Text("Hi"),
+          alignment: Alignment.center,
+        ),
+
+        Container(
+          color: Colors.green,
+          child: Text("Hi"),
+          alignment: Alignment.center,
+        ),
+     
+      ],
+      );
   }
 }
