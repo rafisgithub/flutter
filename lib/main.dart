1efa534b4acbfced9f7c951615aa288df2d7a3b0
Rafi <smrafi120@gmail.com>
2023-07-07 11:58:16 +0600

    All about ListView

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,62 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    title: 'Row and Column Widget',
+    title: "ListView & listTile Widgets",
     home: Scaffold(
-      appBar: AppBar(
-        title: Text("All about Row and Column widget"),
-      ),
+      appBar: AppBar(title: Text("all about ListView and ListTile"),),
       body: MyApp(),
-      
     ),
   ));
 }
 
-class MyApp extends StatelessWidget {
- 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return ListView(
       children: [
-        Text("I "),
-        Text("am "),
-        Text("from "),
-        Text("Row"),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("I "),
-            Text("am "),
-            Text("from"),
-            Text("Column")
-          ],
+        ListTile(
+          title: Text("Item 1"),
+          leading: Container(
+            height: 30,
+            width: 30,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color.fromARGB(255, 121, 241, 121)
+            ),
+          ),
+          trailing: Icon(Icons.arrow_forward),
+        ),
+        ListTile(
+          title: Text("Item 2"),
+          leading: Container(
+            height: 30,
+            width: 30,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color.fromARGB(255, 175, 76, 92)
+            ),
+          ),
+          trailing: Icon(Icons.arrow_forward),
+        ),
+        ListTile(
+          title: Text("Item 3"),
+          leading: Container(
+            height: 30,
+            width: 30,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color.fromARGB(255, 226, 130, 12)
+            ),
+          ),
+          
+          trailing: Icon(Icons.arrow_forward),
         )
       ],
     );
