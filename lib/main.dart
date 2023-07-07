84a6a58a01f9c9d659bd3ab6b9a56e33c93d8e8a
Rafi <smrafi120@gmail.com>
2023-07-07 14:46:19 +0600

    all about Stack widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,143 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: "Stack Example",
+    home: Scaffold(
+      appBar: AppBar(title: Text("Stack Example")),
+      body: MyApp(),
+    ),
+  ));
+}
 
 class MyApp extends StatelessWidget {
-  static final String title = 'ListView Deep Dive';
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: title,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePage(title: title,),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  final String title;
-
-  HomePage({required this.title});
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int index = 0;
-  final items = List.generate(100, (counter) => 'Item: $counter');
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: buildListViews(),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: index,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.view_list),
-            label: 'Basic',
+    return Center(
+      child: Stack(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // alignment: Alignment.center,
+        // overflow:overflow.visiblae
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            height: 200,
+            width: 200,
+            color: Colors.green,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_alt_rounded),
-            label: 'Vertical',
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Color.fromARGB(255, 231, 223, 223),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.line_style),
-            label: 'Horizontal',
+          Positioned(
+            bottom: -25,
+            right: 10,
+            child: Container(
+              height: 50,
+              width: 50,
+              color: const Color.fromARGB(255, 175, 76, 84),
+            ),
           ),
         ],
-        onTap: (int index) => setState(() => this.index = index),
       ),
-    );
-  }
-
-  Widget buildListViews() {
-    if (index == 0) {
-      return buildBasicListView();
-    } else if (index == 1) {
-      return buildVerticalListView();
-    } else if (index == 2) {
-      return buildHorizontalListView();
-    } else {
-      return Container();
-    }
-  }
-
-  // Basic List View Builder method
-  Widget buildBasicListView() {
-    return ListView(
-      children: [
-        ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Colors.brown.shade800,
-            child: Text('AK'),
-          ),
-          title: Text('Opi '),
-          subtitle: Text('Flutter Developer'),
-          trailing: Icon(Icons.star, color: Colors.orange),
-        ),
-        ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Colors.brown.shade800,
-            child: Text('AR'),
-          ),
-          title: Text('Rafi ahmed'),
-          subtitle: Text('Android Developer'),
-          trailing: Icon(Icons.star, color: Colors.orange),
-        ),
-        ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Colors.brown.shade800,
-            child: Text('AB'),
-          ),
-          title: Text('Rafi sheikh'),
-          subtitle: Text('iOS Developer'),
-          trailing: Icon(Icons.star, color: Colors.orange),
-        ),
-      ],
-    );
-  }
-
-  // Vertical List View Builder method
-  Widget buildVerticalListView() {
-    return ListView.separated(
-      separatorBuilder: (context, index) => Divider(
-        color: Colors.black,
-      ),
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(items[index]),
-        );
-      },
-    );
-  }
-
-  // Horizontal List View Builder method
-  Widget buildHorizontalListView() {
-    return ListView.separated(
-      padding: EdgeInsets.all(16),
-      scrollDirection: Axis.horizontal,
-      separatorBuilder: (context, index) => Divider(
-        color: Colors.black,
-      ),
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return Container(
-          width: 100,
-          height: 50,
-          child: ListTile(
-            title: Text(items[index]),
-          ),
-        );
-      },
     );
   }
 }
