574c36f0d6289352e47a9453cd1ebbfbfd447b15
Rafi <smrafi120@gmail.com>
2023-07-07 10:29:02 +0600

    Row & Colmn

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,38 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    title: 'container',
+    title: 'Row and Column Widget',
     home: Scaffold(
+      appBar: AppBar(
+        title: Text("All about Row and Column widget"),
+      ),
       body: MyApp(),
+      
     ),
   ));
 }
 
 class MyApp extends StatelessWidget {
-
+ 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        
-        height: 300,
-        width: 300,
-        child: Text("This is a Container"),
-        alignment: Alignment.center,
-        margin: EdgeInsets.all(30),
-        padding: EdgeInsets.all(30),
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 171, 222, 172),
-          shape: BoxShape.circle,
-          border: Border.all(
-            color: Color.fromARGB(255, 40, 10, 238),
-            width: 10
-          )
-        ),
-        // transform: Matrix4.rotationY(.5),
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("I "),
+        Text("am "),
+        Text("from "),
+        Text("Row"),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("I "),
+            Text("am "),
+            Text("from"),
+            Text("Column")
+          ],
+        )
+      ],
     );
   }
 }
