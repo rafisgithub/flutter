f15ee607ee836e5508436ad3e51e3e9da2d2be26
Rafi <smrafi120@gmail.com>
2023-07-29 20:01:27 +0600

    All about Slider widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,49 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    title: 'flutter Basics',
-    theme: ThemeData(
-      primarySwatch: Colors.amber,
-    ),
+    title: 'Slider Widget',
+    theme: ThemeData(primaryColor: Colors.pink),
     home: Scaffold(
-      appBar: AppBar(
-        title: Text("Radio Button"),
-      ),
+      appBar: AppBar(title: Text('Slider Widget')),
       body: MyApp(),
     ),
   ));
 }
+
 class MyApp extends StatefulWidget {
-
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  // const MyApp({super.key});
-  int _rValue = 0;
-  // void method(int value){
-  //   setState(() {
-  //     _rValue = value;
-  //   });
-  // }
+  double _sValue = 1.0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
       child: Container(
-        decoration: BoxDecoration(border: Border.all(color: Colors.purple)),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.purple),
+        ),
         child: Column(children: [
-         RadioListTile(
-          value: 1, 
-          groupValue: _rValue, 
-          onChanged: (Value){
-            setState(() {
-              _rValue = Value!;
-            });
-          },
-          title: Text('Windows'),
-          subtitle: Text('C'),
-          secondary: Icon(Icons.window),
-          selected: _rValue == 1?true:false,
+          Slider(
+            value: _sValue,
+            min: 0.0,
+            max: 10.0,
+            onChanged: (value) {
+              print(value);
+              setState(() {
+                _sValue = value;
+              });
+            },
+            activeColor: Colors.purple,
+            inactiveColor: Colors.black38,
+            divisions: 10,
+            label: 'Range',
           ),
-         RadioListTile(
-          value: 2, 
-          groupValue: _rValue, 
-          onChanged: (Value){
-             setState(() {
-              _rValue = Value!;
-            });
-          },
-          title: Text('iOS'),
-          subtitle: Text('swift'),
-          secondary: Icon(Icons.apple),
-          selected: _rValue == 2?true:false,
-          ),
-         RadioListTile(
-          value: 3, 
-          groupValue: _rValue, 
-          onChanged: (Value){
-             setState(() {
-              _rValue = Value!;
-            });
-          },
-          title: Text('Adroid'),
-          subtitle: Text('java,C++'),
-          secondary: Icon(Icons.android),
-          selected: _rValue == 3?true:false,
-          ),
+          Text('Range Value : $_sValue')
         ]),
       ),
     );
