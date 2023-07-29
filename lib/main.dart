8e940d56b7fdded2dc7fd8c8b27ccbd9589bdcb5
Rafi <smrafi120@gmail.com>
2023-07-29 16:38:17 +0600

    CheckBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,56 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: 'flutter Basics',
+    theme: ThemeData(
+      primarySwatch: Colors.amber,
+    ),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Checkbox widget"),
+      ),
+      body: MyApp(),
+    ),
+  ));
 }
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-//  late TabController _tabController;
-  @override
-  void initState() {
-    // _tabController = TabController(length: 3, vsync: this);
-    super.initState();
-  }
+class _MyAppState extends State<MyApp> {
+  // const MyApp({super.key});
+  bool _cValue = false;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'flutter Basics',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blue,
-            title: Text('AppBar widget'),
-            centerTitle: true,
-            elevation: 30.0,
-            shadowColor: Colors.black,
-            toolbarOpacity: 0.9,
-            // toolbarHeight: 100,
-            // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          leading: Icon(Icons.menu),
-          actions: [
-            Icon(Icons.favorite),
-            IconButton(
-              onPressed: (){
-                print("search");
-              },
-              icon: Icon(Icons.search),
-            ),
-            Icon(Icons.more_vert),
-          ],
-          bottom: TabBar(
-            // controller: _tabController,
-            tabs: const [
-              Tab(
-                icon: Icon(Icons.person),
-                text: 'Person',
-              ),
-              Tab(
-                icon: Icon(Icons.card_travel),
-                text: 'Travel',
-              ),
-              Tab(
-                icon: Icon(Icons.shopping_cart),
-                text: 'Shooping Card',
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Container(
+        height: 25,
+        width: 25,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.purple)
+        ),
+        child: Transform.scale(
+          scale: 1,
+          child: Checkbox(
+          value: _cValue,
+          onChanged: (status){
+            print(status);
+            setState(() {
+              _cValue = status!;
+            });
             
-            ],
+          },
+          activeColor: Colors.green,
+          checkColor: Colors.red,
+          // tristate: true,
           ),
-          ),
-      
-      
-          body: TabBarView(
-            // controller: _tabController,
-            children: [
-              Container(
-                child: Center(child: Text("Person")),
-              ),
-              Container(
-                child: Center(child: Text("Travel")),
-              ),
-              Container(
-                child: Center(child: Text("Shopping")),
-              ),
-            ],
-            ),
-            
-            
-            bottomNavigationBar: Material(
-              color: Colors.green,
-              child: TabBar(
-                
-                // controller: _tabController,
-                labelColor: Colors.red,
-                unselectedLabelColor:Colors.white,
-                tabs: [
-                  Tab(
-                  icon: Icon(Icons.person),
-                  text: 'Person',
-                ),
-                Tab(
-                  icon: Icon(Icons.card_travel),
-                  text: 'Travel',
-                ),
-                Tab(
-                  icon: Icon(Icons.shopping_cart),
-                  text: 'Shooping Card',
-                ),
-                ],
-              ),
-            ),
         ),
       ),
     );
