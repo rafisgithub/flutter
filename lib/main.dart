a6d5447bfa5e78e292d4025d71013e0ff3014862
Rafi <smrafi120@gmail.com>
2023-07-29 16:05:51 +0600

    DefaultTabController

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,10 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
- late TabController _tabController;
+//  late TabController _tabController;
   @override
   void initState() {
-    _tabController = TabController(length: 3, vsync: this);
+    // _tabController = TabController(length: 3, vsync: this);
     super.initState();
   }
   @override
@@ -25,73 +25,33 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text('AppBar widget'),
-          centerTitle: true,
-          elevation: 30.0,
-          shadowColor: Colors.black,
-          toolbarOpacity: 0.9,
-          // toolbarHeight: 100,
-          // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        leading: Icon(Icons.menu),
-        actions: [
-          Icon(Icons.favorite),
-          IconButton(
-            onPressed: (){
-              print("search");
-            },
-            icon: Icon(Icons.search),
-          ),
-          Icon(Icons.more_vert),
-        ],
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const [
-            Tab(
-              icon: Icon(Icons.person),
-              text: 'Person',
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: Text('AppBar widget'),
+            centerTitle: true,
+            elevation: 30.0,
+            shadowColor: Colors.black,
+            toolbarOpacity: 0.9,
+            // toolbarHeight: 100,
+            // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          leading: Icon(Icons.menu),
+          actions: [
+            Icon(Icons.favorite),
+            IconButton(
+              onPressed: (){
+                print("search");
+              },
+              icon: Icon(Icons.search),
             ),
-            Tab(
-              icon: Icon(Icons.card_travel),
-              text: 'Travel',
-            ),
-            Tab(
-              icon: Icon(Icons.shopping_cart),
-              text: 'Shooping Card',
-            ),
-          
+            Icon(Icons.more_vert),
           ],
-        ),
-        ),
-
-
-        body: TabBarView(
-          controller: _tabController,
-          children: [
-            Container(
-              child: Center(child: Text("Person")),
-            ),
-            Container(
-              child: Center(child: Text("Travel")),
-            ),
-            Container(
-              child: Center(child: Text("Shopping")),
-            ),
-          ],
-          ),
-          
-          
-          bottomNavigationBar: Material(
-            color: Colors.green,
-            child: TabBar(
-              
-              controller: _tabController,
-              labelColor: Colors.red,
-              unselectedLabelColor:Colors.white,
-              tabs: [
-                Tab(
+          bottom: TabBar(
+            // controller: _tabController,
+            tabs: const [
+              Tab(
                 icon: Icon(Icons.person),
                 text: 'Person',
               ),
@@ -103,9 +63,52 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                 icon: Icon(Icons.shopping_cart),
                 text: 'Shooping Card',
               ),
-              ],
-            ),
+            
+            ],
           ),
+          ),
+      
+      
+          body: TabBarView(
+            // controller: _tabController,
+            children: [
+              Container(
+                child: Center(child: Text("Person")),
+              ),
+              Container(
+                child: Center(child: Text("Travel")),
+              ),
+              Container(
+                child: Center(child: Text("Shopping")),
+              ),
+            ],
+            ),
+            
+            
+            bottomNavigationBar: Material(
+              color: Colors.green,
+              child: TabBar(
+                
+                // controller: _tabController,
+                labelColor: Colors.red,
+                unselectedLabelColor:Colors.white,
+                tabs: [
+                  Tab(
+                  icon: Icon(Icons.person),
+                  text: 'Person',
+                ),
+                Tab(
+                  icon: Icon(Icons.card_travel),
+                  text: 'Travel',
+                ),
+                Tab(
+                  icon: Icon(Icons.shopping_cart),
+                  text: 'Shooping Card',
+                ),
+                ],
+              ),
+            ),
+        ),
       ),
     );
   }
