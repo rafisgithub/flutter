cb4811d2a2b9db89d0f783b5bea5dd9c3ac55dae
Rafi <smrafi120@gmail.com>
2023-07-29 15:47:42 +0600

    all about AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,84 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+ late TabController _tabController;
+  @override
+  void initState() {
+    _tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'GridView',
+      title: 'flutter Basics',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: HomePage(title: 'All about Card widget',),
-    );
-  }
-}
-class HomePage extends StatefulWidget {
-  String title;
- HomePage({Key?key,required this.title}):super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("${widget.title}"),
-      ),
-      body: Content(),
-    );
-  }
-}
-
-class Content extends StatefulWidget {
-  const Content({super.key});
-
-  @override
-  State<Content> createState() => _ContentState();
-}
-
-class _ContentState extends State<Content> {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 20,
-      shadowColor: Colors.pink,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      color: Colors.blue,
-      child: Container(
-        height: 300,
-        width: 300,
-      
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('AppBar widget'),
+          centerTitle: true,
+          elevation: 30.0,
+          shadowColor: Colors.black,
+          toolbarOpacity: 0.9,
+          // toolbarHeight: 100,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        leading: Icon(Icons.menu),
+        actions: [
+          Icon(Icons.favorite),
+          IconButton(
+            onPressed: (){
+              print("search");
+            },
+            icon: Icon(Icons.search),
+          ),
+          Icon(Icons.more_vert),
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(
+              icon: Icon(Icons.person),
+              text: 'Person',
+            ),
+            Tab(
+              icon: Icon(Icons.card_travel),
+              text: 'Travel',
+            ),
+            Tab(
+              icon: Icon(Icons.shopping_cart),
+              text: 'Shooping Card',
+            ),
+          
+          ],
+        ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            Container(
+              child: Center(child: Text("Person")),
+            ),
+            Container(
+              child: Center(child: Text("Travel")),
+            ),
+            Container(
+              child: Center(child: Text("Shopping")),
+            ),
+          ]
+          ),
       ),
     );
   }
