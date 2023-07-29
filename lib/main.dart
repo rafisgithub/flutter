cdfc4cdd36e8f90274e6f44616b64c2de15fa694
Rafi <smrafi120@gmail.com>
2023-07-29 21:12:49 +0600

    All about Drawer widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,44 +5,43 @@ void main() {
     title: 'Slider Widget',
     theme: ThemeData(primaryColor: Colors.pink),
     home: Scaffold(
-      appBar: AppBar(title: Text('Switch Widget')),
-      body: MyApp(),
-    ),
-  ));
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  var _slvalue = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(18.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Color.fromARGB(193, 202, 3, 237)),
-        ),
-        child: SwitchListTile(
-          value: _slvalue,
-          onChanged: (value){
-            setState(() {
-              _slvalue = value;
-            });
-          },
-          activeColor: Colors.pink,
-          activeTrackColor: Colors.yellow,
-          inactiveTrackColor: Colors.black,
-        
-          title: Text("Switch"),
-          secondary: Icon(Icons.switch_access_shortcut),
-          selected: _slvalue,
+      appBar: AppBar(title: Text('Drawer Widget')),
+      // body: ,
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            UserAccountsDrawerHeader(
+              accountName: Text('Rafi Ahmed'), 
+              accountEmail: Text('smrafi@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                child: Text('X'),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.pink
+              ),
+              otherAccountsPictures: [
+                CircleAvatar(child: Text('Y'),),
+                CircleAvatar(child: Text('Y'),),
+              ],
+              ),
+            ListTile(
+              title: Text('menu-01'),
+              trailing: Icon(Icons.arrow_back),
+              
+            ),
+            ListTile(
+              title: Text('menu-01'),
+              trailing: Icon(Icons.arrow_back),
+              
+            ),
+            ListTile(
+              title: Text('menu-01'),
+              trailing: Icon(Icons.arrow_back),
+              
+            ),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  ));
 }
